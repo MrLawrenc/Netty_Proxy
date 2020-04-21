4a978aa7958720c16fc5ced813605857d1f8b6d6
刘明瑶-hz20035009 <liumingyao@huize.com>
2020-04-21 18:24:40 +0800

    fix : 重复注册

--- common/src/main/java/com/swust/common/handler/CommonHandler.java
+++ common/src/main/java/com/swust/common/handler/CommonHandler.java
@@ -1,13 +1,7 @@
 package com.swust.common.handler;
 
-import com.swust.common.exception.ProxyException;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import lombok.Getter;
-
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.logging.Logger;
 
 /**
  * @author : LiuMing
@@ -15,47 +9,11 @@ import java.util.logging.Logger;
  * @description :   公共handler
  */
 public class CommonHandler extends ChannelInboundHandlerAdapter {
-    protected Logger logger = Logger.getGlobal();
-
-    protected AtomicInteger lossConnectCount = new AtomicInteger(0);
-    @Getter
-    protected ChannelHandlerContext ctx;
-
-
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        this.ctx = ctx;
-    }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        if (cause instanceof ProxyException) {
-            logger.info("ProxyException.......................");
-        } else {
-            logger.warning(String.format("client/server exception(%s) will close!...............", cause.getMessage()));
-            Channel channel = ctx.channel();
-            logger.warning("#### exceptionCaught #### " + String.format("localAddr:%s  remoteAddr:%s", channel.localAddress(), channel.remoteAddress()) + " obj->" + this.getClass().getSimpleName());
-            ctx.close();
-        }
+        cause.printStackTrace();
+        ctx.close();
     }
 
-    @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        /*if (evt instanceof IdleStateEvent) {
-            IdleStateEvent e = (IdleStateEvent) evt;
-            if (e.state() == IdleState.READER_IDLE) {
-                int retryNum = lossConnectCount.incrementAndGet();
-                if (retryNum > DEFAULT_RECONNECTION_LIMIT) {
-                    logger.severe("Read idle  will loss connection. retryNum:" + retryNum);
-                    ctx.close();
-                }
-            } else if (e.state() == IdleState.WRITER_IDLE) {
-                Message message = new Message();
-                message.getHeader().setType(MessageType.KEEPALIVE);
-                ctx.writeAndFlush(message);
-            }
-        } else {
-            super.userEventTriggered(ctx, evt);
-        }*/
-    }
 }
